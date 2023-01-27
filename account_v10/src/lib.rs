1027716e364536064baab7c9f2f565724465c555
tyshkor <tyshko1@gmail.com>
2023-01-27 11:55:07 +0100

    most changes

---
+++ account_v10/src/lib.rs
@@ -0,0 +1,23 @@
+// Copyright 2019-2022 ChainSafe Systems
+// SPDX-License-Identifier: Apache-2.0, MIT
+
+use fvm_shared::METHOD_CONSTRUCTOR;
+use num_derive::FromPrimitive;
+
+pub use self::state::State;
+
+mod state;
+pub mod testing;
+pub mod types;
+
+/// Account actor methods available
+#[derive(FromPrimitive)]
+#[repr(u64)]
+pub enum Method {
+    Constructor = METHOD_CONSTRUCTOR,
+    PubkeyAddress = 2,
+    // Deprecated in v10
+    // AuthenticateMessage = 3,
+    AuthenticateMessageExported = frc42_dispatch::method_hash!("AuthenticateMessage"),
+    UniversalReceiverHook = frc42_dispatch::method_hash!("Receive"),
+}
