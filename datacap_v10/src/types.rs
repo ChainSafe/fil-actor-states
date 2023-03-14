d0961bd33849ddc6fd09ac615e531e32d967c5fb
Shashank <99187193+sudo-shashank@users.noreply.github.com>
2023-03-14 18:31:06 +0530

    Added new v10 builtin actors (#30)

---
+++ datacap_v10/src/types.rs
@@ -0,0 +1,28 @@
+// Copyright 2019-2022 ChainSafe Systems
+// SPDX-License-Identifier: Apache-2.0, MIT
+
+use fvm_ipld_encoding::tuple::*;
+use fvm_shared::address::Address;
+use fvm_shared::econ::TokenAmount;
+
+#[derive(Clone, Debug, PartialEq, Eq, Serialize_tuple, Deserialize_tuple)]
+pub struct MintParams {
+    // Recipient of the newly minted tokens.
+    pub to: Address,
+    // Amount of tokens to mint.
+    pub amount: TokenAmount,
+    // Addresses to be granted effectively-infinite operator allowance for the recipient.
+    pub operators: Vec<Address>,
+}
+
+#[derive(Clone, Debug, PartialEq, Eq, Serialize_tuple, Deserialize_tuple)]
+pub struct DestroyParams {
+    pub owner: Address,
+    pub amount: TokenAmount,
+}
+
+#[derive(Clone, Debug, PartialEq, Eq, Serialize_tuple, Deserialize_tuple)]
+#[serde(transparent)]
+pub struct GranularityReturn {
+    pub granularity: u64,
+}
