b2113e5bc5931fd2685c11101527c02827a15f73
hanabi1224 <harlowmoo@gmail.com>
2023-03-20 21:47:24 +0800

    Regression test for dll deps

---
+++ fil_actor_interface/tests/dummy.rs
@@ -0,0 +1,6 @@
+#[cfg(test)]
+mod tests {
+    /// This test is used to ensure dependencies on CI are sufficient, to avoid regressions in bringing opencl back
+    #[test]
+    fn ensure_no_missing_dll() {}
+}
