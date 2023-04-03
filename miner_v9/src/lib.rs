09ee978a3a9a4ab1c8865c878fa4e020a14c09da
sudo-shashank <shashank@chainsafe.io>
2023-04-03 15:28:20 +0530

    Cleanup

--- miner_v9/src/lib.rs
+++ miner_v9/src/lib.rs
@@ -53,8 +53,6 @@ mod vesting_state;
 // The first 1000 actor-specific codes are left open for user error, i.e. things that might
 // actually happen without programming error in the actor code.
 
-// * Updated to specs-actors commit: 17d3c602059e5c48407fb3c34343da87e6ea6586 (v0.9.12)
-
 /// Storage Miner actor methods available
 #[derive(FromPrimitive)]
 #[repr(u64)]
