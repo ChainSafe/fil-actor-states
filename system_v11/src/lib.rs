63466b4aea3cc99040576abc4e7c9879f961068d
David Himmelstrup <lemmih@gmail.com>
2023-04-19 15:50:04 +0200

    update dependencies for system_v11

--- system_v11/src/lib.rs
+++ system_v11/src/lib.rs
@@ -1,14 +1,9 @@
 // Copyright 2019-2022 ChainSafe Systems
 // SPDX-License-Identifier: Apache-2.0, MIT
-use cid::{multihash, Cid};
-use fvm_ipld_blockstore::Blockstore;
+use cid::Cid;
 use fvm_ipld_encoding::tuple::*;
-use fvm_ipld_encoding::CborStore;
-use fvm_shared::error::ExitCode;
 use fvm_shared::METHOD_CONSTRUCTOR;
 use num_derive::FromPrimitive;
-
-use fil_actors_runtime_v11::{ActorError, AsActorError};
 
 /// System actor methods.
 #[derive(FromPrimitive)]
@@ -24,22 +19,4 @@ pub struct State {
     pub builtin_actors: Cid,
 }
 
-impl State {
-    pub fn new<BS: Blockstore>(store: &BS) -> Result<Self, ActorError> {
-        let c = store
-            .put_cbor(&Vec::<(String, Cid)>::new(), multihash::Code::Blake2b256)
-            .context_code(ExitCode::USR_ILLEGAL_STATE, "failed to store system state")?;
-        Ok(Self { builtin_actors: c })
-    }
-
-    pub fn get_builtin_actors<B: Blockstore>(
-        &self,
-        store: &B,
-    ) -> Result<Vec<(String, Cid)>, String> {
-        match store.get_cbor(&self.builtin_actors) {
-            Ok(Some(obj)) => Ok(obj),
-            Ok(None) => Err("failed to load builtin actor registry; not found".to_string()),
-            Err(e) => Err(e.to_string()),
-        }
-    }
-}
+impl State {}
