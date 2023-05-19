ef7177178d921997db4ab7f74057b7d74708b7f9
sudo-shashank <shashank@chainsafe.io>
2023-05-19 13:25:09 +0530

    run fmt

--- actors/datacap/src/v9/state.rs
+++ actors/datacap/src/v9/state.rs
@@ -1,9 +1,9 @@
+use fil_actors_shared::v9::{ActorError, AsActorError};
 use frc46_token::token;
 use fvm_ipld_blockstore::Blockstore;
 use fvm_ipld_encoding::tuple::*;
 use fvm_shared::address::Address;
 use fvm_shared::error::ExitCode;
-use fil_actors_shared::v9::{ActorError, AsActorError};
 
 #[derive(Serialize_tuple, Deserialize_tuple)]
 pub struct State {
@@ -15,7 +15,9 @@ impl State {
     pub fn new<BS: Blockstore>(store: &BS, governor: Address) -> Result<State, ActorError> {
         let token_state = token::state::TokenState::new(store)
             .context_code(ExitCode::USR_ILLEGAL_STATE, "failed to create token state")?;
-        Ok(State { governor, token: token_state })
+        Ok(State {
+            governor,
+            token: token_state,
+        })
     }
 }
-
