d090972a217f886f7c562441e6e8ad64ff3dcbed
sudo-shashank <shashank@chainsafe.io>
2023-06-01 13:40:09 +0530

    Remove macro use

--- fil_actors_shared/src/v8/builtin/singletons.rs
+++ fil_actors_shared/src/v8/builtin/singletons.rs
@@ -3,29 +3,17 @@
 
 use fvm_shared::address::Address;
 use fvm_shared::ActorID;
-use paste::paste;
 
-macro_rules! define_builtin_addr {
-    ($($name:ident = $id:literal,)*) => {
-        $(
-            paste! {
-                pub const [<$name _ADDR>]: Address = Address::new_id([<$id>]);
-            }
-        )*
-    }
-}
-
-define_builtin_addr! {
-    SYSTEM_ACTOR = 0,
-    INIT_ACTOR = 1,
-    REWARD_ACTOR = 2,
-    CRON_ACTOR = 3,
-    STORAGE_POWER_ACTOR = 4,
-    STORAGE_MARKET_ACTOR = 5,
-    VERIFIED_REGISTRY_ACTOR = 6,
-    CHAOS_ACTOR = 98,
-    BURNT_FUNDS_ACTOR = 99,
-}
+/// Define Builtin Actor Addresses
+pub const SYSTEM_ACTOR_ADDR: Address = Address::new_id(0);
+pub const INIT_ACTOR_ADDR: Address = Address::new_id(1);
+pub const REWARD_ACTOR_ADDR: Address = Address::new_id(2);
+pub const CRON_ACTOR_ADDR: Address = Address::new_id(3);
+pub const STORAGE_POWER_ACTOR_ADDR: Address = Address::new_id(4);
+pub const STORAGE_MARKET_ACTOR_ADDR: Address = Address::new_id(5);
+pub const VERIFIED_REGISTRY_ACTOR_ADDR: Address = Address::new_id(6);
+pub const CHAOS_ACTOR_ADDR: Address = Address::new_id(98);
+pub const BURNT_FUNDS_ACTOR_ADDR: Address = Address::new_id(99);
 
 /// Defines first available ID address after builtin actors
 pub const FIRST_NON_SINGLETON_ADDR: ActorID = 100;
