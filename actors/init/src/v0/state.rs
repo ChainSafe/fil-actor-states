812a030e98c6cd6c6fc2b805ec9d3462b0c279e8
Rahul Sharma <rahul@chainsafe.io>
2023-06-23 14:25:23 +0000

    add resolve_address method on v0 init actor

--- actors/init/src/v0/state.rs
+++ actors/init/src/v0/state.rs
@@ -2,8 +2,13 @@
 // SPDX-License-Identifier: Apache-2.0, MIT
 
 use cid::Cid;
+use fil_actors_shared::v9::{make_map_with_root_and_bitwidth, shared::HAMT_BIT_WIDTH};
+use fvm_ipld_blockstore::Blockstore;
 use fvm_ipld_encoding::tuple::*;
-use fvm_shared::ActorID;
+use fvm_shared::{
+    address::{Address, Protocol},
+    ActorID,
+};
 
 /// Defines first available ID address after builtin actors
 pub const FIRST_NON_SINGLETON_ADDR: ActorID = 100;
@@ -23,5 +28,29 @@ impl State {
             next_id: FIRST_NON_SINGLETON_ADDR,
             network_name,
         }
+    }
+
+    /// `ResolveAddress` resolves an address to an ID-address, if possible.
+    /// If the provided address is an ID address, it is returned as-is.
+    /// This means that mapped ID-addresses (which should only appear as values, not keys) and
+    /// singleton actor addresses (which are not in the map) pass through unchanged.
+    ///
+    /// Returns an ID-address and `true` if the address was already an ID-address or was resolved
+    /// in the mapping.
+    /// Returns an undefined address and `false` if the address was not an ID-address and not found
+    /// in the mapping.
+    /// Returns an error only if state was inconsistent.
+    pub fn resolve_address<BS: Blockstore>(
+        &self,
+        store: &BS,
+        addr: &Address,
+    ) -> anyhow::Result<Option<Address>> {
+        if addr.protocol() == Protocol::ID {
+            return Ok(Some(*addr));
+        }
+
+        let map = make_map_with_root_and_bitwidth(&self.address_map, store, HAMT_BIT_WIDTH)?;
+
+        Ok(map.get(&addr.to_bytes())?.copied().map(Address::new_id))
     }
 }
