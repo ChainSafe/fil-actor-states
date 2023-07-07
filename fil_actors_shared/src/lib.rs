d6b84514ed13bf0a43d8102e5cfa37adb6eb121c
David Himmelstrup <david.himmelstrup@chainsafe.io>
2023-07-07 11:15:21 +0200

    expose TokenState::new and TokenState::new_with_bit_width

--- fil_actors_shared/src/lib.rs
+++ fil_actors_shared/src/lib.rs
@@ -10,8 +10,27 @@ pub mod v9;
 pub mod frc46_token {
     use cid::Cid;
     use fvm_ipld_encoding::tuple::*;
+    use fvm_ipld_hamt::Hamt;
+    use fvm_ipld_blockstore::Blockstore;
     use fvm_shared3::econ::TokenAmount;
+    use fvm_ipld_hamt::{BytesKey, Error as HamtError};
 
+    type Map<'bs, BS, K, V> = Hamt<&'bs BS, V, K>;
+    type BalanceMap<'bs, BS> = Map<'bs, BS, BytesKey, TokenAmount>;
+    type AllowanceMap<'bs, BS> = Map<'bs, BS, BytesKey, Cid>;
+
+    type Result<T> = std::result::Result<T, HamtError>;
+
+    /// This value has been chosen to optimise to reduce gas-costs when accessing the balances map. Non-
+    /// standard use cases of the token library might find a different value to be more efficient.
+    pub const DEFAULT_HAMT_BIT_WIDTH: u32 = 3;
+
+    /// An abstraction over the IPLD layer to get and modify token state without dealing with HAMTs etc.
+    ///
+    /// This is a simple wrapper of state and in general does not account for token protocol level
+    /// checks such as ensuring necessary approvals are enforced during transfers. This is left for the
+    /// caller to handle. However, some invariants such as non-negative balances, allowances and total
+    /// supply are enforced.
     #[derive(Serialize_tuple, Deserialize_tuple, PartialEq, Eq, Clone, Debug)]
     pub struct TokenState {
         /// Total supply of token
@@ -22,5 +41,31 @@ pub mod frc46_token {
         pub allowances: Cid,
         /// Bit-width to use when loading Hamts
         hamt_bit_width: u32,
+    }
+
+    impl TokenState {
+        /// Create a new token state-tree, without committing it (the root cid) to a blockstore
+        pub fn new<BS: Blockstore>(store: &BS) -> Result<Self> {
+            Self::new_with_bit_width(store, DEFAULT_HAMT_BIT_WIDTH)
+        }
+
+        /// Create a new token state-tree, without committing it (the root cid) to a blockstore
+        ///
+        /// Explicitly sets the bit width of underlying Hamt structures. Caller must ensure
+        /// 1 <= hamt_bit_width <= 8.
+        pub fn new_with_bit_width<BS: Blockstore>(store: &BS, hamt_bit_width: u32) -> Result<Self> {
+            // Blockstore is still needed to create valid Cids for the Hamts
+            let empty_balance_map =
+                BalanceMap::new_with_bit_width(store, hamt_bit_width).flush()?;
+            let empty_allowances_map =
+                AllowanceMap::new_with_bit_width(store, hamt_bit_width).flush()?;
+
+            Ok(Self {
+                supply: Default::default(),
+                balances: empty_balance_map,
+                allowances: empty_allowances_map,
+                hamt_bit_width,
+            })
+        }
     }
 }
