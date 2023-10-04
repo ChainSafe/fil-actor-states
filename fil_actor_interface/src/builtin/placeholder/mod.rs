e2f88fb7982d93bebcbbf432daba1fe7982d31fb
sudo-shashank <shashank@chainsafe.io>
2023-10-04 18:58:00 +0530

    Added placeholder and ethaccount

--- fil_actor_interface/src/builtin/placeholder/mod.rs
+++ fil_actor_interface/src/builtin/placeholder/mod.rs
@@ -10,3 +10,7 @@ pub fn is_v10_placeholder_cid(cid: &Cid) -> bool {
 pub fn is_v11_placeholder_cid(cid: &Cid) -> bool {
     crate::KNOWN_CIDS.actor.placeholder.v11.contains(cid)
 }
+
+pub fn is_v12_placeholder_cid(cid: &Cid) -> bool {
+    crate::KNOWN_CIDS.actor.placeholder.v12.contains(cid)
+}
