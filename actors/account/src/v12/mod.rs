16e11f29a5884e0830a5e73bcd83a0d83381651b
sudo-shashank <shashank@chainsafe.io>
2023-10-04 11:19:19 +0530

    Added account v12 actor

---
+++ actors/account/src/v12/mod.rs
@@ -0,0 +1,22 @@
+// Copyright 2019-2022 ChainSafe Systems
+// SPDX-License-Identifier: Apache-2.0, MIT
+
+use fvm_shared3::METHOD_CONSTRUCTOR;
+use num_derive::FromPrimitive;
+
+pub use self::state::State;
+pub use types::*;
+
+mod state;
+mod types;
+
+/// Account actor methods available
+#[derive(FromPrimitive)]
+#[repr(u64)]
+pub enum Method {
+    Constructor = METHOD_CONSTRUCTOR,
+    PubkeyAddress = 2,
+    // Deprecated in v10
+    // AuthenticateMessage = 3,
+    AuthenticateMessageExported = frc42_dispatch::method_hash!("AuthenticateMessage"),
+}
