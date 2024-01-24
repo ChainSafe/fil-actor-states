e8d1f73f9bb7d1743e3e926bc7392ba4ea1894d0
Josh Jones <joshua.jones@chainsafe.io>
2024-01-24 09:36:57 +0000

    Changes to market state for Forest `StateMarketStorageDeal` RPC method (#229)

--- fil_actor_interface/src/builtin/market/mod.rs
+++ fil_actor_interface/src/builtin/market/mod.rs
@@ -1,12 +1,30 @@
 // Copyright 2019-2023 ChainSafe Systems
 // SPDX-License-Identifier: Apache-2.0, MIT
 
-use crate::convert::{from_token_v3_to_v2, from_token_v4_to_v2};
+use crate::convert::{
+    from_address_v3_to_v2, from_address_v4_to_v2, from_padded_piece_size_v3_to_v2,
+    from_padded_piece_size_v4_to_v2, from_token_v3_to_v2, from_token_v4_to_v2,
+};
 use anyhow::Context;
 use cid::Cid;
+use fil_actor_market_state::v10::DealArray as V10DealArray;
+use fil_actor_market_state::v10::DealMetaArray as V10DealMetaArray;
+use fil_actor_market_state::v11::DealArray as V11DealArray;
+use fil_actor_market_state::v11::DealMetaArray as V11DealMetaArray;
+use fil_actor_market_state::v12::DealArray as V12DealArray;
+use fil_actor_market_state::v12::DealMetaArray as V12DealMetaArray;
+use fil_actor_market_state::v9::DealArray as V9DealArray;
+use fil_actor_market_state::v9::DealMetaArray as V9DealMetaArray;
+use fil_actors_shared::v10::AsActorError as V10AsActorError;
+use fil_actors_shared::v11::AsActorError as V11AsActorError;
+use fil_actors_shared::v12::AsActorError as V12AsActorError;
+use fil_actors_shared::v9::AsActorError as V9AsActorError;
 use fvm_ipld_blockstore::Blockstore;
+use fvm_shared::error::ExitCode as FVMExitCode;
 use fvm_shared::{address::Address, clock::ChainEpoch, econ::TokenAmount, piece::PaddedPieceSize};
-use serde::Serialize;
+use fvm_shared3::error::ExitCode as FVM3ExitCode;
+use fvm_shared4::error::ExitCode as FVM4ExitCode;
+use serde::{Deserialize, Serialize};
 use std::marker::PhantomData;
 
 use crate::io::get_obj;
@@ -16,6 +34,8 @@ pub const ADDRESS: Address = Address::new_id(5);
 
 /// Market actor method.
 pub type Method = fil_actor_market_state::v8::Method;
+
+pub type AllocationID = u64;
 
 pub fn is_v8_market_cid(cid: &Cid) -> bool {
     crate::KNOWN_CIDS.actor.market.v8.contains(cid)
@@ -98,19 +118,50 @@ impl State {
     }
 
     /// Deal proposals
-    pub fn proposals<'bs, BS>(&self, _store: &'bs BS) -> anyhow::Result<DealProposals<'bs, BS>>
+    pub fn proposals<'bs, BS>(&'bs self, store: &'bs BS) -> anyhow::Result<DealProposals<'bs, BS>>
     where
         BS: Blockstore,
     {
-        unimplemented!()
+        match self {
+            // `get_proposal_array` does not exist for V8
+            State::V8(_st) => anyhow::bail!("unimplemented"),
+            // `get_proposal_array` does not exist for V9
+            State::V9(_st) => anyhow::bail!("unimplemented"),
+            State::V10(st) => Ok(DealProposals::V10(st.get_proposal_array(store)?)),
+            State::V11(st) => Ok(DealProposals::V11(st.get_proposal_array(store)?)),
+            State::V12(st) => Ok(DealProposals::V12(st.get_proposal_array(store)?)),
+        }
     }
 
     /// Deal proposal meta data.
-    pub fn states<'bs, BS>(&self, _store: &'bs BS) -> anyhow::Result<DealStates<'bs, BS>>
+    pub fn states<'bs, BS>(&self, store: &'bs BS) -> anyhow::Result<DealStates<'bs, BS>>
     where
         BS: Blockstore,
     {
-        unimplemented!()
+        match self {
+            // `DealMetaArray::load` does not exist for V8
+            State::V8(_st) => anyhow::bail!("unimplemented"),
+            State::V9(st) => Ok(DealStates::V9(V9AsActorError::context_code(
+                V9DealMetaArray::load(&st.states, store),
+                FVMExitCode::USR_ILLEGAL_STATE,
+                "failed to load deal state array",
+            )?)),
+            State::V10(st) => Ok(DealStates::V10(V10AsActorError::context_code(
+                V10DealMetaArray::load(&st.states, store),
+                FVM3ExitCode::USR_ILLEGAL_STATE,
+                "failed to load deal state array",
+            )?)),
+            State::V11(st) => Ok(DealStates::V11(V11AsActorError::context_code(
+                V11DealMetaArray::load(&st.states, store),
+                FVM3ExitCode::USR_ILLEGAL_STATE,
+                "failed to load deal state array",
+            )?)),
+            State::V12(st) => Ok(DealStates::V12(V12AsActorError::context_code(
+                V12DealMetaArray::load(&st.states, store),
+                FVM4ExitCode::USR_ILLEGAL_STATE,
+                "failed to load deal state array",
+            )?)),
+        }
     }
 
     /// Consume state to return just total funds locked
@@ -129,23 +180,35 @@ pub enum BalanceTable<'a, BS> {
     UnusedBalanceTable(PhantomData<&'a BS>),
 }
 
-pub enum DealProposals<'a, BS> {
-    UnusedDealProposal(PhantomData<&'a BS>),
+pub enum DealProposals<'bs, BS> {
+    V9(V9DealArray<'bs, BS>),
+    V10(V10DealArray<'bs, BS>),
+    V11(V11DealArray<'bs, BS>),
+    V12(V12DealArray<'bs, BS>),
 }
 
 impl<BS> DealProposals<'_, BS> {
     pub fn for_each(
         &self,
-        _f: impl FnMut(u64, DealProposal) -> anyhow::Result<(), anyhow::Error>,
+        mut f: impl FnMut(u64, Result<DealProposal, anyhow::Error>) -> anyhow::Result<(), anyhow::Error>,
     ) -> anyhow::Result<()>
     where
         BS: Blockstore,
     {
-        unimplemented!()
+        match self {
+            DealProposals::V9(deal_array) => Ok(deal_array
+                .for_each(|key, deal_proposal| f(key, DealProposal::try_from(deal_proposal)))?),
+            DealProposals::V10(deal_array) => Ok(deal_array
+                .for_each(|key, deal_proposal| f(key, DealProposal::try_from(deal_proposal)))?),
+            DealProposals::V11(deal_array) => Ok(deal_array
+                .for_each(|key, deal_proposal| f(key, DealProposal::try_from(deal_proposal)))?),
+            DealProposals::V12(deal_array) => Ok(deal_array
+                .for_each(|key, deal_proposal| f(key, DealProposal::try_from(deal_proposal)))?),
+        }
     }
 }
 
-#[derive(Serialize)]
+#[derive(Clone, Serialize, Deserialize, PartialEq)]
 #[serde(rename_all = "PascalCase")]
 pub struct DealProposal {
     #[serde(rename = "PieceCID")]
@@ -163,25 +226,167 @@ pub struct DealProposal {
     pub client_collateral: TokenAmount,
 }
 
-pub enum DealStates<'a, BS> {
-    DealStates(PhantomData<&'a BS>),
+impl TryFrom<&fil_actor_market_state::v9::DealProposal> for DealProposal {
+    type Error = anyhow::Error;
+
+    fn try_from(
+        deal_proposal: &fil_actor_market_state::v9::DealProposal,
+    ) -> Result<Self, Self::Error> {
+        Ok(Self {
+            piece_cid: deal_proposal.piece_cid,
+            piece_size: deal_proposal.piece_size,
+            verified_deal: deal_proposal.verified_deal,
+            client: deal_proposal.client,
+            provider: deal_proposal.provider,
+            label: match &deal_proposal.label {
+                fil_actor_market_state::v9::Label::String(s) => s.clone(),
+                fil_actor_market_state::v9::Label::Bytes(b) => String::from_utf8(b.clone())?,
+            },
+            start_epoch: deal_proposal.start_epoch,
+            end_epoch: deal_proposal.end_epoch,
+            storage_price_per_epoch: deal_proposal.storage_price_per_epoch.clone(),
+            provider_collateral: deal_proposal.provider_collateral.clone(),
+            client_collateral: deal_proposal.client_collateral.clone(),
+        })
+    }
+}
+
+impl TryFrom<&fil_actor_market_state::v10::DealProposal> for DealProposal {
+    type Error = anyhow::Error;
+
+    fn try_from(
+        deal_proposal: &fil_actor_market_state::v10::DealProposal,
+    ) -> Result<Self, Self::Error> {
+        Ok(Self {
+            piece_cid: deal_proposal.piece_cid,
+            piece_size: from_padded_piece_size_v3_to_v2(deal_proposal.piece_size),
+            verified_deal: deal_proposal.verified_deal,
+            client: from_address_v3_to_v2(deal_proposal.client),
+            provider: from_address_v3_to_v2(deal_proposal.provider),
+            label: match &deal_proposal.label {
+                fil_actor_market_state::v10::Label::String(s) => s.clone(),
+                fil_actor_market_state::v10::Label::Bytes(b) => String::from_utf8(b.clone())?,
+            },
+            start_epoch: deal_proposal.start_epoch,
+            end_epoch: deal_proposal.end_epoch,
+            storage_price_per_epoch: from_token_v3_to_v2(
+                deal_proposal.storage_price_per_epoch.clone(),
+            ),
+            provider_collateral: from_token_v3_to_v2(deal_proposal.provider_collateral.clone()),
+            client_collateral: from_token_v3_to_v2(deal_proposal.client_collateral.clone()),
+        })
+    }
+}
+
+impl TryFrom<&fil_actor_market_state::v11::DealProposal> for DealProposal {
+    type Error = anyhow::Error;
+
+    fn try_from(
+        deal_proposal: &fil_actor_market_state::v11::DealProposal,
+    ) -> Result<Self, Self::Error> {
+        Ok(Self {
+            piece_cid: deal_proposal.piece_cid,
+            piece_size: from_padded_piece_size_v3_to_v2(deal_proposal.piece_size),
+            verified_deal: deal_proposal.verified_deal,
+            client: from_address_v3_to_v2(deal_proposal.client),
+            provider: from_address_v3_to_v2(deal_proposal.provider),
+            label: match &deal_proposal.label {
+                fil_actor_market_state::v11::Label::String(s) => s.clone(),
+                fil_actor_market_state::v11::Label::Bytes(b) => String::from_utf8(b.clone())?,
+            },
+            start_epoch: deal_proposal.start_epoch,
+            end_epoch: deal_proposal.end_epoch,
+            storage_price_per_epoch: from_token_v3_to_v2(
+                deal_proposal.storage_price_per_epoch.clone(),
+            ),
+            provider_collateral: from_token_v3_to_v2(deal_proposal.provider_collateral.clone()),
+            client_collateral: from_token_v3_to_v2(deal_proposal.client_collateral.clone()),
+        })
+    }
+}
+
+impl TryFrom<&fil_actor_market_state::v12::DealProposal> for DealProposal {
+    type Error = anyhow::Error;
+
+    fn try_from(
+        deal_proposal: &fil_actor_market_state::v12::DealProposal,
+    ) -> Result<Self, Self::Error> {
+        Ok(Self {
+            piece_cid: deal_proposal.piece_cid,
+            piece_size: from_padded_piece_size_v4_to_v2(deal_proposal.piece_size),
+            verified_deal: deal_proposal.verified_deal,
+            client: from_address_v4_to_v2(deal_proposal.client),
+            provider: from_address_v4_to_v2(deal_proposal.provider),
+            label: match &deal_proposal.label {
+                fil_actor_market_state::v12::Label::String(s) => s.clone(),
+                fil_actor_market_state::v12::Label::Bytes(b) => String::from_utf8(b.clone())?,
+            },
+            start_epoch: deal_proposal.start_epoch,
+            end_epoch: deal_proposal.end_epoch,
+            storage_price_per_epoch: from_token_v4_to_v2(
+                deal_proposal.storage_price_per_epoch.clone(),
+            ),
+            provider_collateral: from_token_v4_to_v2(deal_proposal.provider_collateral.clone()),
+            client_collateral: from_token_v4_to_v2(deal_proposal.client_collateral.clone()),
+        })
+    }
+}
+
+pub enum DealStates<'bs, BS> {
+    V8(V9DealMetaArray<'bs, BS>),
+    V9(V9DealMetaArray<'bs, BS>),
+    V10(V10DealMetaArray<'bs, BS>),
+    V11(V11DealMetaArray<'bs, BS>),
+    V12(V12DealMetaArray<'bs, BS>),
 }
 
 impl<BS> DealStates<'_, BS>
 where
     BS: Blockstore,
 {
-    pub fn get(&self, _key: u64) -> anyhow::Result<Option<DealState>> {
-        unimplemented!()
+    pub fn get(&self, key: u64) -> anyhow::Result<Option<DealState>> {
+        match self {
+            DealStates::V8(deal_array) => Ok(deal_array.get(key)?.map(|deal_state| DealState {
+                sector_start_epoch: deal_state.sector_start_epoch,
+                last_updated_epoch: deal_state.last_updated_epoch,
+                slash_epoch: deal_state.slash_epoch,
+                verified_claim: deal_state.verified_claim,
+            })),
+            DealStates::V9(deal_array) => Ok(deal_array.get(key)?.map(|deal_state| DealState {
+                sector_start_epoch: deal_state.sector_start_epoch,
+                last_updated_epoch: deal_state.last_updated_epoch,
+                slash_epoch: deal_state.slash_epoch,
+                verified_claim: deal_state.verified_claim,
+            })),
+            DealStates::V10(deal_array) => Ok(deal_array.get(key)?.map(|deal_state| DealState {
+                sector_start_epoch: deal_state.sector_start_epoch,
+                last_updated_epoch: deal_state.last_updated_epoch,
+                slash_epoch: deal_state.slash_epoch,
+                verified_claim: deal_state.verified_claim,
+            })),
+            DealStates::V11(deal_array) => Ok(deal_array.get(key)?.map(|deal_state| DealState {
+                sector_start_epoch: deal_state.sector_start_epoch,
+                last_updated_epoch: deal_state.last_updated_epoch,
+                slash_epoch: deal_state.slash_epoch,
+                verified_claim: deal_state.verified_claim,
+            })),
+            DealStates::V12(deal_array) => Ok(deal_array.get(key)?.map(|deal_state| DealState {
+                sector_start_epoch: deal_state.sector_start_epoch,
+                last_updated_epoch: deal_state.last_updated_epoch,
+                slash_epoch: deal_state.slash_epoch,
+                verified_claim: deal_state.verified_claim,
+            })),
+        }
     }
 }
 
-#[derive(Serialize)]
+#[derive(Clone, Serialize, Deserialize, PartialEq)]
 #[serde(rename_all = "PascalCase")]
 pub struct DealState {
     pub sector_start_epoch: ChainEpoch, // -1 if not yet included in proven sector
     pub last_updated_epoch: ChainEpoch, // -1 if deal state never updated
     pub slash_epoch: ChainEpoch,        // -1 if deal never slashed
+    pub verified_claim: AllocationID, // ID of the verified registry allocation/claim for this deal's data (0 if none).
 }
 
 impl<BS> BalanceTable<'_, BS>
