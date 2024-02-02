49b9ef1b52c258b43b0d45d5d6b8e58fea00cb4b
sudo-shashank <shashank@chainsafe.io>
2024-02-02 11:40:15 +0530

    added v13 reward actor

---
+++ actors/reward/src/v13/mod.rs
@@ -0,0 +1,31 @@
+// Copyright 2019-2022 ChainSafe Systems
+// SPDX-License-Identifier: Apache-2.0, MIT
+
+use fvm_shared4::METHOD_CONSTRUCTOR;
+use num_derive::FromPrimitive;
+
+pub use self::logic::*;
+pub use self::state::{Reward, State, VestingFunction};
+pub use self::types::*;
+
+pub(crate) mod expneg;
+mod logic;
+mod state;
+mod types;
+
+// only exported for tests
+#[doc(hidden)]
+pub mod ext;
+
+/// PenaltyMultiplier is the factor miner penalties are scaled up by
+pub const PENALTY_MULTIPLIER: u64 = 3;
+
+/// Reward actor methods available
+#[derive(FromPrimitive)]
+#[repr(u64)]
+pub enum Method {
+    Constructor = METHOD_CONSTRUCTOR,
+    AwardBlockReward = 2,
+    ThisEpochReward = 3,
+    UpdateNetworkKPI = 4,
+}
