4591e99d54c6e5fc3f478599c50e1f07687cab44
sudo-shashank <shashank@chainsafe.io>
2024-02-02 08:43:20 +0530

    added v13 runtime verbatimly

---
+++ fil_actors_shared/src/v13/runtime/actor_code.rs
@@ -0,0 +1,28 @@
+// Copyright 2019-2022 ChainSafe Systems
+// SPDX-License-Identifier: Apache-2.0, MIT
+
+use fvm_ipld_blockstore::Blockstore;
+use fvm_ipld_encoding::ipld_block::IpldBlock;
+use fvm_shared4::MethodNum;
+
+use crate::v13::{ActorError, Runtime};
+
+/// Interface for invoking methods on an Actor
+pub trait ActorCode {
+    type Methods;
+    /// A name for the actor type, used in debugging.
+    fn name() -> &'static str;
+    /// Invokes method with runtime on the actor's code. Method number will match one
+    /// defined by the Actor, and parameters will be serialized and used in execution
+    fn invoke_method<RT>(
+        rt: &RT,
+        method: MethodNum,
+        params: Option<IpldBlock>,
+    ) -> Result<Option<IpldBlock>, ActorError>
+    where
+        // TODO: remove the clone requirement on the blockstore when we fix "replica update" to not
+        // hold onto state between transactions.
+        // https://github.com/filecoin-project/builtin-actors/issues/133
+        RT: Runtime,
+        RT::Blockstore: Blockstore + Clone;
+}
