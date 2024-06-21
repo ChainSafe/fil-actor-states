21d9a71b565e07327b15863af860f406311b03d0
sudo-shashank <shashank@chainsafe.io>
2024-06-21 13:14:48 +0530

    Added datacap actor v14

---
+++ actors/datacap/src/v14/state.rs
@@ -0,0 +1,37 @@
+use frc46_token::token;
+use fvm_ipld_blockstore::Blockstore;
+use fvm_ipld_encoding::tuple::*;
+use fvm_shared4::address::Address;
+use fvm_shared4::econ::TokenAmount;
+use fvm_shared4::error::ExitCode;
+use fvm_shared4::ActorID;
+
+use fil_actors_shared::v13::{ActorError, AsActorError};
+
+#[derive(Debug, Serialize_tuple, Deserialize_tuple)]
+pub struct State {
+    pub governor: Address,
+    pub token: token::state::TokenState,
+}
+
+impl State {
+    pub fn new<BS: Blockstore>(store: &BS, governor: Address) -> Result<State, ActorError> {
+        let token_state = token::state::TokenState::new(store)
+            .context_code(ExitCode::USR_ILLEGAL_STATE, "failed to create token state")?;
+        Ok(State {
+            governor,
+            token: token_state,
+        })
+    }
+
+    // Visible for testing
+    pub fn balance<BS: Blockstore>(
+        &self,
+        bs: &BS,
+        owner: ActorID,
+    ) -> Result<TokenAmount, ActorError> {
+        self.token
+            .get_balance(bs, owner)
+            .context_code(ExitCode::USR_ILLEGAL_STATE, "failed to get balance")
+    }
+}
---
+++ actors/datacap/src/v14/types.rs
@@ -0,0 +1,85 @@
+use fvm_ipld_encoding::tuple::*;
+use fvm_shared4::address::Address;
+use fvm_shared4::econ::TokenAmount;
+
+#[derive(Clone, Debug, PartialEq, Eq, Serialize_tuple, Deserialize_tuple)]
+#[serde(transparent)]
+pub struct ConstructorParams {
+    pub governor: Address,
+}
+
+#[derive(Clone, Debug, PartialEq, Eq, Serialize_tuple, Deserialize_tuple)]
+#[serde(transparent)]
+pub struct NameReturn {
+    pub name: String,
+}
+
+#[derive(Clone, Debug, PartialEq, Eq, Serialize_tuple, Deserialize_tuple)]
+#[serde(transparent)]
+pub struct SymbolReturn {
+    pub symbol: String,
+}
+
+#[derive(Clone, Debug, PartialEq, Eq, Serialize_tuple, Deserialize_tuple)]
+#[serde(transparent)]
+pub struct TotalSupplyReturn {
+    pub supply: TokenAmount,
+}
+
+#[derive(Clone, Debug, PartialEq, Eq, Serialize_tuple, Deserialize_tuple)]
+#[serde(transparent)]
+pub struct BalanceParams {
+    pub address: Address,
+}
+
+#[derive(Clone, Debug, PartialEq, Eq, Serialize_tuple, Deserialize_tuple)]
+#[serde(transparent)]
+pub struct BalanceReturn {
+    pub balance: TokenAmount,
+}
+
+#[derive(Clone, Debug, PartialEq, Eq, Serialize_tuple, Deserialize_tuple)]
+#[serde(transparent)]
+pub struct GetAllowanceReturn {
+    pub allowance: TokenAmount,
+}
+
+#[derive(Clone, Debug, PartialEq, Eq, Serialize_tuple, Deserialize_tuple)]
+#[serde(transparent)]
+pub struct IncreaseAllowanceReturn {
+    pub new_allowance: TokenAmount,
+}
+
+#[derive(Clone, Debug, PartialEq, Eq, Serialize_tuple, Deserialize_tuple)]
+#[serde(transparent)]
+pub struct DecreaseAllowanceReturn {
+    pub new_allowance: TokenAmount,
+}
+
+#[derive(Clone, Debug, PartialEq, Eq, Serialize_tuple, Deserialize_tuple)]
+#[serde(transparent)]
+pub struct RevokeAllowanceReturn {
+    pub old_allowance: TokenAmount,
+}
+
+#[derive(Clone, Debug, PartialEq, Eq, Serialize_tuple, Deserialize_tuple)]
+pub struct MintParams {
+    // Recipient of the newly minted tokens.
+    pub to: Address,
+    // Amount of tokens to mint.
+    pub amount: TokenAmount,
+    // Addresses to be granted effectively-infinite operator allowance for the recipient.
+    pub operators: Vec<Address>,
+}
+
+#[derive(Clone, Debug, PartialEq, Eq, Serialize_tuple, Deserialize_tuple)]
+pub struct DestroyParams {
+    pub owner: Address,
+    pub amount: TokenAmount,
+}
+
+#[derive(Clone, Debug, PartialEq, Eq, Serialize_tuple, Deserialize_tuple)]
+#[serde(transparent)]
+pub struct GranularityReturn {
+    pub granularity: u64,
+}
