18793526e40586d923914ebd8f2e370dae13cb52
sudo-shashank <shashank@chainsafe.io>
2024-06-21 18:54:19 +0530

    Added verifreg actor v14

---
+++ actors/verifreg/src/v14/mod.rs
@@ -0,0 +1,43 @@
+// Copyright 2019-2022 ChainSafe Systems
+// SPDX-License-Identifier: Apache-2.0, MIT
+
+use fvm_shared4::METHOD_CONSTRUCTOR;
+use num_derive::FromPrimitive;
+
+pub use self::state::Allocation;
+pub use self::state::Claim;
+pub use self::state::State;
+pub use self::types::*;
+
+#[cfg(feature = "fil-actor")]
+fil_actors_shared::v14::wasm_trampoline!(Actor);
+
+pub mod expiration;
+pub mod ext;
+pub mod state;
+pub mod types;
+
+/// Account actor methods available
+#[derive(FromPrimitive)]
+#[repr(u64)]
+pub enum Method {
+    Constructor = METHOD_CONSTRUCTOR,
+    AddVerifier = 2,
+    RemoveVerifier = 3,
+    AddVerifiedClient = 4,
+    // UseBytes = 5,     // Deprecated
+    // RestoreBytes = 6, // Deprecated
+    RemoveVerifiedClientDataCap = 7,
+    RemoveExpiredAllocations = 8,
+    ClaimAllocations = 9,
+    GetClaims = 10,
+    ExtendClaimTerms = 11,
+    RemoveExpiredClaims = 12,
+    // Method numbers derived from FRC-0042 standards
+    AddVerifiedClientExported = frc42_dispatch::method_hash!("AddVerifiedClient"),
+    RemoveExpiredAllocationsExported = frc42_dispatch::method_hash!("RemoveExpiredAllocations"),
+    GetClaimsExported = frc42_dispatch::method_hash!("GetClaims"),
+    ExtendClaimTermsExported = frc42_dispatch::method_hash!("ExtendClaimTerms"),
+    RemoveExpiredClaimsExported = frc42_dispatch::method_hash!("RemoveExpiredClaims"),
+    UniversalReceiverHook = frc42_dispatch::method_hash!("Receive"),
+}
