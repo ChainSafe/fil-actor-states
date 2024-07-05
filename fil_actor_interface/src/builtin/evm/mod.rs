0d31ce537b4565e5975b2eebe974cbb86974328f
sudo-shashank <shashank@chainsafe.io>
2024-07-05 08:48:33 +0530

    add is_alive method

--- fil_actor_interface/src/builtin/evm/mod.rs
+++ fil_actor_interface/src/builtin/evm/mod.rs
@@ -74,4 +74,14 @@ impl State {
         }
         Err(anyhow::anyhow!("Unknown evm actor code {}", code))
     }
+
+    pub fn is_alive(&self) -> bool {
+        match self {
+            State::V10(st) => st.tombstone.is_none(),
+            State::V11(st) => st.tombstone.is_none(),
+            State::V12(st) => st.tombstone.is_none(),
+            State::V13(st) => st.tombstone.is_none(),
+            State::V14(st) => st.tombstone.is_none(),
+        }
+    }
 }
