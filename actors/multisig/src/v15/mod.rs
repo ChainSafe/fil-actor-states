9018e9b0d33bcf5d13ce9e8688ec4065c4ec6ae9
sudo-shashank <shashank@chainsafe.io>
2024-10-04 15:36:11 +0530

    Add multisig actor v15

---
+++ actors/multisig/src/v15/mod.rs
@@ -0,0 +1,28 @@
+// Copyright 2019-2022 ChainSafe Systems
+// SPDX-License-Identifier: Apache-2.0, MIT
+
+use fvm_shared4::METHOD_CONSTRUCTOR;
+use num_derive::FromPrimitive;
+
+pub use self::state::*;
+pub use self::types::*;
+
+mod state;
+mod types;
+
+/// Multisig actor methods available
+#[derive(FromPrimitive)]
+#[repr(u64)]
+pub enum Method {
+    Constructor = METHOD_CONSTRUCTOR,
+    Propose = 2,
+    Approve = 3,
+    Cancel = 4,
+    AddSigner = 5,
+    RemoveSigner = 6,
+    SwapSigner = 7,
+    ChangeNumApprovalsThreshold = 8,
+    LockBalance = 9,
+    // Method numbers derived from FRC-0042 standards
+    UniversalReceiverHook = frc42_macros::method_hash!("Receive"),
+}
