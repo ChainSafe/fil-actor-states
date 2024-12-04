13aa9c64d7274891cecfbfb76cb8730529a9ecd6
sudo-shashank <shashank@chainsafe.io>
2024-12-04 10:24:24 +0530

    Added eam types

--- actors/eam/src/v12/mod.rs
+++ actors/eam/src/v12/mod.rs
@@ -1,8 +1,11 @@
 // Copyright 2019-2022 ChainSafe Systems
 // SPDX-License-Identifier: Apache-2.0, MIT
 
-use fvm_shared4::METHOD_CONSTRUCTOR;
+use fil_actor_evm_state::evm_shared::v12::address::EthAddress;
+use fvm_ipld_encoding::{strict_bytes, tuple::*};
+use fvm_shared4::{address::Address, ActorID, METHOD_CONSTRUCTOR};
 use num_derive::FromPrimitive;
+use serde::{Deserialize, Serialize};
 
 #[derive(FromPrimitive)]
 #[repr(u64)]
@@ -12,3 +15,33 @@ pub enum Method {
     Create2 = 3,
     CreateExternal = 4,
 }
+
+#[derive(Serialize_tuple, Deserialize_tuple)]
+pub struct CreateParams {
+    #[serde(with = "strict_bytes")]
+    pub initcode: Vec<u8>,
+    pub nonce: u64,
+}
+
+#[derive(Serialize_tuple, Deserialize_tuple)]
+pub struct Create2Params {
+    #[serde(with = "strict_bytes")]
+    pub initcode: Vec<u8>,
+    #[serde(with = "strict_bytes")]
+    pub salt: [u8; 32],
+}
+
+#[derive(Serialize, Deserialize)]
+#[serde(transparent)]
+pub struct CreateExternalParams(#[serde(with = "strict_bytes")] pub Vec<u8>);
+
+#[derive(Serialize_tuple, Deserialize_tuple, Debug, PartialEq, Eq)]
+pub struct Return {
+    pub actor_id: ActorID,
+    pub robust_address: Option<Address>,
+    pub eth_address: EthAddress,
+}
+
+pub type CreateReturn = Return;
+pub type Create2Return = Return;
+pub type CreateExternalReturn = Return;
