0f9010d9f9c3ab07282a33faa05be374ea654b58
sudo-shashank <shashank@chainsafe.io>
2024-12-05 08:12:11 +0530

    Add paych v16

---
+++ actors/paych/src/v16/mod.rs
@@ -0,0 +1,25 @@
+// Copyright 2019-2022 ChainSafe Systems
+// SPDX-License-Identifier: Apache-2.0, MIT
+
+use fvm_shared4::error::ExitCode;
+use fvm_shared4::METHOD_CONSTRUCTOR;
+use num_derive::FromPrimitive;
+
+pub use self::state::*;
+pub use self::types::*;
+
+pub mod ext;
+mod state;
+mod types;
+
+/// Payment Channel actor methods available
+#[derive(FromPrimitive)]
+#[repr(u64)]
+pub enum Method {
+    Constructor = METHOD_CONSTRUCTOR,
+    UpdateChannelState = 2,
+    Settle = 3,
+    Collect = 4,
+}
+
+pub const ERR_CHANNEL_STATE_UPDATE_AFTER_SETTLED: ExitCode = ExitCode::new(32);
