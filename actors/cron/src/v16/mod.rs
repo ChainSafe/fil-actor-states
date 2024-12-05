3ccc6a21a9df2f3a0f0b1d12ba8be2e7b10b703a
sudo-shashank <shashank@chainsafe.io>
2024-12-05 07:05:31 +0530

    Add cron v16

---
+++ actors/cron/src/v16/mod.rs
@@ -0,0 +1,27 @@
+// Copyright 2019-2022 ChainSafe Systems
+// SPDX-License-Identifier: Apache-2.0, MIT
+
+use fvm_ipld_encoding::tuple::*;
+
+use fvm_shared4::METHOD_CONSTRUCTOR;
+use num_derive::FromPrimitive;
+
+pub use self::state::{Entry, State};
+
+mod state;
+
+/// Cron actor methods available
+#[derive(FromPrimitive)]
+#[repr(u64)]
+pub enum Method {
+    Constructor = METHOD_CONSTRUCTOR,
+    EpochTick = 2,
+}
+
+/// Constructor parameters for Cron actor, contains entries
+/// of actors and methods to call on each epoch
+#[derive(Default, Debug, Serialize_tuple, Deserialize_tuple)]
+pub struct ConstructorParams {
+    /// Entries is a set of actors (and corresponding methods) to call during EpochTick.
+    pub entries: Vec<Entry>,
+}
