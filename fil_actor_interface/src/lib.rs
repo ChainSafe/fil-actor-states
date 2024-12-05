ee9fe74a7eaf3ac199049b7e70d45b03b4d3f228
Shashank <99187193+sudo-shashank@users.noreply.github.com>
2024-12-05 21:58:51 +0530

    Remove fil_actor_interface crate and sync check (#349)

--- fil_actor_interface/src/lib.rs
+++
@@ -1,9 +0,0 @@
-// Copyright 2019-2023 ChainSafe Systems
-// SPDX-License-Identifier: Apache-2.0, MIT
-
-mod builtin;
-pub mod convert;
-mod macros;
-
-pub use self::builtin::*;
-pub use fil_actors_shared::v13::runtime::Policy;
