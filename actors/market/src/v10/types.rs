394f5a769e6c40b31fb43e24a9cb69c27ca4ad4f
elmattic <elmattic@users.noreply.github.com>
2025-08-21 14:25:29 +0200

    Add missing types

--- actors/market/src/v10/types.rs
+++ actors/market/src/v10/types.rs
@@ -24,6 +24,12 @@ pub const PROPOSALS_AMT_BITWIDTH: u32 = 5;
 pub const STATES_AMT_BITWIDTH: u32 = 6;
 
 #[derive(Serialize_tuple, Deserialize_tuple, Debug, Clone, Eq, PartialEq)]
+#[serde(transparent)]
+pub struct AddBalanceParams {
+    pub provider_or_client: Address,
+}
+
+#[derive(Serialize_tuple, Deserialize_tuple, Debug, Clone, Eq, PartialEq)]
 pub struct WithdrawBalanceParams {
     pub provider_or_client: Address,
     pub amount: TokenAmount,
